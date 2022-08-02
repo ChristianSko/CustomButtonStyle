2a3f4ae94c61df4a4dad99f3d3a5ccf5d4275fdf
Christian Skorobogatow <cskorobogatow@deloitte.es>
2022-08-02 10:27:07 +0200

    Initial Commit

---
+++ CustomButtonStyle/CustomButtonStyleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomButtonStyleApp.swift
+//  CustomButtonStyle
+//
+//  Created by Skorobogatow, Christian on 2/8/22.
+//
+
+import SwiftUI
+
+@main
+struct CustomButtonStyleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
